662bd70662fc03bc915464886e5670adc74d3f25
sabit1997 <yejiii1997@gmail.com>
2023-04-06 16:50:34 +0900

    Feat: SideBar 컴포넌트 추가

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,13 @@
+export default function SideBar() {
+  return (
+    <div className="sidebar">
+      <h2 className="sidebar-title">ToDo</h2>
+      <ul className="sidebar-list">
+        <li className="sidebar-menu">Home</li>
+        <li className="sidebar-menu">Sign In</li>
+        <li className="sidebar-menu">Sign Up</li>
+      </ul>
+      <div>User Profile</div>
+    </div>
+  );
+}
