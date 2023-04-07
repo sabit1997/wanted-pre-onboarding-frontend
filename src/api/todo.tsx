dc3f55d1404c6b4089baa64af020119c9ec95900
sabit1997 <yejiii1997@gmail.com>
2023-04-08 00:38:56 +0900

    Feat: todo api 구현

---
+++ src/api/todo.tsx
@@ -0,0 +1,55 @@
+import { BASE_URL } from './const';
+
+interface TodoRequest {
+  task: string;
+}
+const access_token = localStorage.getItem('token');
+
+export const createTodo = async (args: TodoRequest) => {
+  const createTodoRes = await fetch(`${BASE_URL}todos`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${access_token}`,
+    },
+    body: JSON.stringify(args),
+  });
+};
+
+export const getTodos = async (id: number) => {
+  const getTodosRes = await fetch(`${BASE_URL}todos`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+  const TodosData = await getTodosRes.json();
+  return TodosData;
+};
+
+interface UpdateRequest {
+  todo: string;
+  isCompleted: boolean;
+}
+
+export const updateTodo = async (id: number, args: UpdateRequest) => {
+  const updateTodoRes = await fetch(`${BASE_URL}todos/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${access_token}`,
+    },
+    body: JSON.stringify(args),
+  });
+};
+
+export const deleteTodo = async (id: number) => {
+  const deleteTodoRes = await fetch(`${BASE_URL}todos/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+};
