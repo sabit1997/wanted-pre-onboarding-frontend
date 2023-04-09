be87be75bcecbbe519c2fcd9398f190b1b9d5a8c
sabit1997 <yejiii1997@gmail.com>
2023-04-09 22:07:10 +0900

    Fix: style 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="page-wrapper">
       <h1 className="page-title">Home</h1>
-      <button className="form-button">로그인하러 가기</button>
+      <button className="button move-button">로그인하러 가기</button>
     </div>
   );
 }
